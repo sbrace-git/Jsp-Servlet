88ef0cf57d7ace607bdebff90f486d3236d3eefd
wly <wly>
2022-02-16 17:09:51 +0800

    WebServlet values

---
+++ ch02/FirstServlet/src/main/java/cc/openhome/Path2.java
@@ -0,0 +1,24 @@
+package cc.openhome;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({"/path1/*", "/path2/*"})
+public class Path2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.printf("requestURL = %s%n", req.getRequestURL());
+        System.out.printf("pathInfo = %s%n", req.getPathInfo());
+        System.out.printf("servletPath = %s%n", req.getServletPath());
+        System.out.printf("servletName = %s%n", getServletName());
+        HttpServletMapping httpServletMapping = req.getHttpServletMapping();
+        System.out.printf("MappingMatch = %s%n", httpServletMapping.getMappingMatch());
+        System.out.printf("MatchValue = %s%n", httpServletMapping.getMatchValue());
+        System.out.printf("Pattern = %s%n", httpServletMapping.getPattern());
+    }
+}
