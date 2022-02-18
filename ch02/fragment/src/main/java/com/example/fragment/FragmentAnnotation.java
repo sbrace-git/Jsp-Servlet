e5e4e8758daf42a8708929898895212c64f7f771
sbrace <sbrace0822@gmail.com>
2022-02-18 15:38:56 +0800

    add test servlet

---
+++ ch02/fragment/src/main/java/com/example/fragment/FragmentAnnotation.java
@@ -0,0 +1,16 @@
+package com.example.fragment;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/fragmentAnnotation")
+public class FragmentAnnotation extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain; charset=UTF-8");
+        resp.getWriter().print(getServletName());
+    }
+}
--- ch02/fragment/src/main/java/com/example/fragment/FragmentConfig.java
+++ ch02/fragment/src/main/java/com/example/fragment/FragmentConfig.java
@@ -1,16 +1,15 @@
 package com.example.fragment;
 
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/fragment")
-public class Fragment extends HttpServlet {
+public class FragmentConfig extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain; charset=UTF-8");
-        resp.getWriter().print("fragment");
+        resp.getWriter().print(getServletName());
     }
 }
