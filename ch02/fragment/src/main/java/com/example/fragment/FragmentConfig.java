cd1f17e966f9f99528ecb24b00be671d34fad889
sbrace <sbrace0822@gmail.com>
2022-02-21 11:33:00 +0800

    fragment complete

--- ch02/fragment/src/main/java/com/example/fragment/FragmentConfig.java
+++ ch02/fragment/src/main/java/com/example/fragment/FragmentConfig.java
@@ -1,14 +1,19 @@
 package com.example.fragment;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class FragmentConfig extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void init() {
+        System.out.println("FragmentConfig init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain; charset=UTF-8");
         resp.getWriter().print(getServletName());
     }
