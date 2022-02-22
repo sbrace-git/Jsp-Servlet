171899125355aef1e8e8b050c4cb0449c4d6e554
sbrace <sbrace0822@gmail.com>
2022-02-22 17:24:43 +0800

    ch02 FirstServlet - atack update

--- ch02/FirstServlet/src/main/java/cc/openhome/AttackServlet.java
+++ ch02/FirstServlet/src/main/java/cc/openhome/AttackServlet.java
@@ -7,14 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 
-@WebServlet(name = "atack",value = "/atack")
-public class AtackServlet extends HttpServlet {
+@WebServlet(name = "attack", value = "/attack")
+public class AttackServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String name = request.getParameter("name");
+        String name = Optional.of(request.getParameter("name"))
+                .map(nameTemp -> nameTemp.replace("<", "&lt;")
+                                         .replace(">", "&gt;"))
+                .orElse("Guest");
         PrintWriter writer = response.getWriter();
         writer.print("<!DOCTYPE html>");
         writer.print("<html>");
@@ -22,7 +27,7 @@ public class AtackServlet extends HttpServlet {
         writer.print("<title>Hello</title>");
         writer.print("</head>");
         writer.print("<body>");
-        writer.printf("<h1> Hello! %s! %n</h1>", name);
+        writer.printf("<h1> Hello! %s!</h1>", name);
         writer.print("</body>");
         writer.print("</html>");
     }
