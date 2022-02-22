6d45aa7852e8ee1d7caeac9b2554b2d7a158033e
sbrace <sbrace0822@gmail.com>
2022-02-22 17:51:55 +0800

    ch03 header - print headers

---
+++ ch03/header/src/main/java/com/example/header/Header.java
@@ -0,0 +1,31 @@
+package com.example.header;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
+
+@WebServlet("/header")
+public class Header extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+
+        writer.print("<!DOCTYPE html>");
+        writer.print("<html>");
+        writer.print("<head>");
+        writer.print("<title>");
+        writer.print("header test page");
+        writer.print("</title>");
+        writer.print("</head>");
+        writer.print("<body>");
+        Collections.list(req.getHeaderNames())
+                .forEach(headerName -> writer.printf("%s : %s</br>", headerName, req.getHeader(headerName)));
+        writer.print("</body>");
+        writer.print("</html>");
+    }
+}
