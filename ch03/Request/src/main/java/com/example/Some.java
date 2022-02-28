1aac190d5aacbf4ce7823e2ac92ef6bc1f5b571e
wly <wly>
2022-02-28 22:11:26 +0800

    ch03 Request - include forward

--- ch03/Request/src/main/java/com/example/Some.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-import javax.servlet.http.HttpServlet;
-
-public class Some extends HttpServlet {
-}
---
+++ ch03/Request/src/main/java/com/example/dispatcher/Book.java
@@ -0,0 +1,36 @@
+package com.example.dispatcher;
+
+public class Book {
+    private String name;
+
+    private String author;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
