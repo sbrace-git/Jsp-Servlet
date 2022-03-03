6d1aa654e748b9b05103789595234fe218da9c6d
sbrace <sbrace0822@gmail.com>
2022-03-03 17:59:06 +0800

    ch03 Response - DefaultCharSet

--- ch03/Response/src/main/java/cc/openhome/response/charset/DefaultCharSet.java
+++ ch03/Response/src/main/java/cc/openhome/response/charset/DefaultCharSet.java
@@ -11,6 +11,16 @@ import java.io.IOException;
 public class DefaultCharSet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        /**
+         * see
+         * src/main/webapp/WEB-INF/web.xml
+         * <request-character-encoding>
+         * <response-character-encoding>
+         */
+        System.out.printf("contentType = %s%n", req.getContentType());
+        System.out.printf("characterEncoding = %s%n", req.getCharacterEncoding());
+
         System.out.printf("ContentType = %s%n", resp.getContentType());
         System.out.printf("CharacterEncoding = %s%n", resp.getCharacterEncoding());
         resp.getWriter().println("Hello world! 你好，世界！");
