e9a96d1a9cf4fe08e62c63f711c365d105feaec4
wly <wly>
2022-03-18 21:23:37 +0800

    ch04 gossip - member.view update

--- ch04/gossip/src/main/java/cc/openhome/gossip/view/MemberView.java
+++ ch04/gossip/src/main/java/cc/openhome/gossip/view/MemberView.java
@@ -7,7 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 @WebServlet("/member.view")
 public class MemberView extends HttpServlet {
@@ -30,6 +36,62 @@ public class MemberView extends HttpServlet {
             resp.sendRedirect(LOGIN_PATH);
             return;
         }
+
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<!DOCTYPE html>");
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<meta charset='UTF-8'/>");
+        writer.println("<title>Gossip 微博</title>");
+        writer.println("<link rel='stylesheet' href='css/member.css' type='text/css' />");
+        writer.println("</head>");
+
+        writer.println("<body>");
+        writer.println("<div class='leftPanel'>");
+        writer.println("<img src='images/caterpillar.jpg' alt='Gossip 微博' /> <br/>");
+        writer.printf("<a href='logout'>注销 %s</a>", username);
+        writer.println("</div>");
+
+        writer.println("<form method='post' action='new_message'>");
+        writer.println("分享新鲜事...<br/>");
+
+        String preBlabla = req.getParameter("blabla");
+        if (null == preBlabla) {
+            preBlabla = "";
+        } else {
+            writer.println("讯息要在140字内<br/>");
+        }
+        writer.printf("<textarea cols='60' rows='4' name='blabla'>%s</textarea>", preBlabla);
+        writer.println("</form>");
+
+        Map<Long, String> messages = (Map<Long, String>) req.getAttribute("messages");
+        if (null == messages || messages.isEmpty()) {
+            writer.println("写点什么吧...");
+        } else {
+            writer.println("<table border='0' cellpadding='2' cellspacing='2'>");
+            writer.println("<thead>");
+            writer.println("<tr><th><hr/></th></tr>");
+            writer.println("</thead>");
+            writer.println("<tbody>");
+
+            for (Map.Entry<Long, String> message : messages.entrySet()) {
+                Long millis = message.getKey();
+                String blabla = message.getValue();
+
+                LocalDateTime localDateTime = Instant.ofEpochMilli(millis)
+                        .atZone(ZoneId.systemDefault()).toLocalDateTime();
+                writer.println("<tr><td style='vertical-align: top;'>");
+                writer.println("");
+            }
+
+            writer.println("</tbody>");
+            writer.println("</table>");
+        }
+
+        writer.println("</body>");
+        writer.println("</html>");
+
 
     }
 
