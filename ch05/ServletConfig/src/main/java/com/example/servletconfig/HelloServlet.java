a33a621c2b7d5b71b1bcb2438cc876dd9faf2c86
wly <wly>
2022-03-20 01:44:40 +0800

    ch05 ServletConfig - WebInitParam

--- ch05/ServletConfig/src/main/java/com/example/servletconfig/HelloServlet.java
+++ ch05/ServletConfig/src/main/java/com/example/servletconfig/HelloServlet.java
@@ -1,24 +1,37 @@
 package com.example.servletconfig;
 
 import java.io.*;
+import java.util.Collections;
+import java.util.Enumeration;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello-servlet",
+        initParams = {
+                @WebInitParam(name = "message", value = "你好!"),
+                @WebInitParam(name = "param", value = "这是内容")
+        })
 public class HelloServlet extends HttpServlet {
     private String message;
+    private String text;
 
     public void init() {
-        message = "Hello World!";
+        System.out.printf("%s init", getClass().getName());
+        text = getInitParameter("param");
+        message = getInitParameter("message");
+        Collections.list(getInitParameterNames()).forEach(initParameterName -> {
+            System.out.printf("%s = %s%n", initParameterName, getInitParameter(initParameterName));
+        });
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        response.setContentType("text/html; charset=UTF-8");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + text + "</h1>");
         out.println("</body></html>");
     }
 
