945c0caec8a8fd7f9771a9481f8865a1629b6af8
wly <wly>
2022-03-20 12:33:47 +0800

    ch05 ServletConfig - Login

---
+++ ch05/ServletConfig/src/main/java/com/example/servletconfig/ErrorView.java
@@ -0,0 +1,17 @@
+package com.example.servletconfig;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/error.view")
+public class ErrorView extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain; charset=UTF-8");
+        resp.getWriter().println("login error");
+    }
+}
