d4f57ed341ba07ee4948b368e38472ccd66762e3
sbrace <sbrace0822@gmail.com>
2022-03-24 23:31:00 +0800

    ch05 listener - HttpSessionActivationListener

--- ch05/listener/src/main/java/com/example/listener/session/User.java
+++ ch05/listener/src/main/java/com/example/listener/session/User.java
@@ -1,18 +1,31 @@
 package com.example.listener.session;
 
+import javax.servlet.http.HttpSessionActivationListener;
 import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionBindingListener;
+import javax.servlet.http.HttpSessionEvent;
+import java.io.Serializable;
 
-public class User implements HttpSessionBindingListener {
+public class User implements HttpSessionBindingListener, HttpSessionActivationListener, Serializable {
 
     private String name;
     private Integer age;
 
     @Override
+    public void sessionWillPassivate(HttpSessionEvent se) {
+        System.out.printf("sessionWillPassivate %s%n", this);
+    }
+
+    @Override
+    public void sessionDidActivate(HttpSessionEvent se) {
+        System.out.printf("sessionDidActivate %s%n", this);
+    }
+
+    @Override
     public void valueBound(HttpSessionBindingEvent event) {
         String name = event.getName();
         Object value = event.getValue();
-        System.out.printf("MySessionBindingListener # valueBound name = %s, value = %s%n", name, value);
+        System.out.printf("User # valueBound name = %s, value = %s%n", name, value);
     }
 
     @Override
@@ -21,7 +34,7 @@ public class User implements HttpSessionBindingListener {
         Object value = event.getValue();
         // replace : value = null. 先执行 valueBound 再执行 valueUnbound
         // remove : value = removed value
-        System.out.printf("MySessionBindingListener # valueUnbound name = %s, value = %s%n", name, value);
+        System.out.printf("User # valueUnbound name = %s, value = %s%n", name, value);
     }
 
     public User() {
