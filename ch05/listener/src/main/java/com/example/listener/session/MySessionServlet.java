3ad1a950119519066f1d2b302738b3473472b931
wly <wly>
2022-03-24 22:15:43 +0800

    ch05 listener - HttpSessionBindingListener

--- ch05/listener/src/main/java/com/example/listener/session/MySessionServlet.java
+++ ch05/listener/src/main/java/com/example/listener/session/MySessionServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @WebServlet("/update-session")
 public class MySessionServlet extends HttpServlet {
@@ -13,10 +14,15 @@ public class MySessionServlet extends HttpServlet {
         HttpSession session = req.getSession();
         String name = req.getParameter("name");
         String value = req.getParameter("value");
+        Integer age = Optional.ofNullable(req.getParameter("age"))
+                .map(Integer::valueOf)
+                .orElse(null);
         if (null == value) {
             session.removeAttribute(name);
+            session.removeAttribute("user");
         } else {
             session.setAttribute(name, value);
+            session.setAttribute("user", new User(name, age));
         }
     }
 }
