575316b6127bb59cd67cbb043fe23b323d85afa8
wly <wly>
2022-03-26 15:11:59 +0800

    ch05 listener - session id changed listener

---
+++ ch05/listener/src/main/java/com/example/listener/session/ChangeSessionIdServlet.java
@@ -0,0 +1,23 @@
+package com.example.listener.session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/changeSessionId")
+public class ChangeSessionIdServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            req.changeSessionId();
+        } else {
+            session = req.getSession(true);
+        }
+        session.setAttribute("test","test");
+    }
+}
