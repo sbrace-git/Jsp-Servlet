904bd116be6e687b15cc420ba51119f448852b85
sbrace <sbrace0822@gmail.com>
2022-03-27 16:23:17 +0800

    ch05 filters - request parameter html encoding

---
+++ ch05/filters/src/main/java/com/example/filters/EchoServlet.java
@@ -0,0 +1,18 @@
+package com.example.filters;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/echo")
+public class EchoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String content = req.getParameter("content");
+        resp.setContentType("text/plain");
+        resp.getWriter().println(content);
+    }
+}
