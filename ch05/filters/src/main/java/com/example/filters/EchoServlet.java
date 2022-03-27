209329a265ff23978c23a6b0eee42823f6162d64
sbrace <sbrace0822@gmail.com>
2022-03-27 18:08:30 +0800

    ch05 filters - gzip response

--- ch05/filters/src/main/java/com/example/filters/EchoServlet.java
+++ ch05/filters/src/main/java/com/example/filters/EchoServlet.java
@@ -1,11 +1,11 @@
 package com.example.filters;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/echo")
 public class EchoServlet extends HttpServlet {
@@ -13,6 +13,7 @@ public class EchoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String content = req.getParameter("content");
         resp.setContentType("text/plain");
-        resp.getWriter().println(content);
+        PrintWriter writer = resp.getWriter();
+        writer.println(content);
     }
 }
