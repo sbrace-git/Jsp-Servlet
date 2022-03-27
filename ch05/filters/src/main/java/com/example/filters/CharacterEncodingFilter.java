3a8073783c35551a0c28b6c6dbecd94c289c69f4
wly <wly>
2022-03-27 16:30:31 +0800

    ch05 filter - CharacterEncodingFilter

--- ch05/filters/src/main/java/com/example/filters/CharacterEncodingFilter.java
+++ ch05/filters/src/main/java/com/example/filters/CharacterEncodingFilter.java
@@ -10,13 +10,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+/**
+ * Servlet 4.0 之后，若是整个 Web 应用程序都要采用的默认编码，可以在 web.xml 中设定
+ * <request-character-encoding></request-character-encoding>
+ * <response-character-encoding></response-character-encoding>
+ * 如果几个特有的 servlet 需要使用特定编码，可以用过滤器的方式实现
+ */
 @WebFilter(
         urlPatterns = "/*",
         initParams = {
                 @WebInitParam(name = "encoding", value = "UTF-8")
         }
 )
-public class RequestCharacterEncodingFilter extends HttpFilter {
+public class CharacterEncodingFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
             throws IOException, ServletException {
