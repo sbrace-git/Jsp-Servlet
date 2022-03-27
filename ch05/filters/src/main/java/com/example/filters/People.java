2de8357d5c08f6ca38dd088bb352b887c4a8b0e1
sbrace <sbrace0822@gmail.com>
2022-03-27 15:53:36 +0800

    ch05 filter - 测试继承没有无参构造方法的类

---
+++ ch05/filters/src/main/java/com/example/filters/People.java
@@ -0,0 +1,20 @@
+package com.example.filters;
+
+public class People {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public People(String name) {
+        this.name = name;
+    }
+
+    public People() {
+    }
+}
---
+++ ch05/filters/src/main/java/com/example/filters/Person.java
@@ -0,0 +1,9 @@
+package com.example.filters;
+
+/**
+ * 如果 People 没有无参构造方法， Person 编译报错
+ */
+public class Person extends People {
+
+
+}
