6624696b97b6be425796ce73e87a1eab576e8950
sbrace <sbrace0822@gmail.com>
2022-03-31 00:17:18 +0800

    ch05 async - asyncContext add listener

--- ch05/async/src/main/java/com/example/async/longPolling/AsyncNumber.java
+++ ch05/async/src/main/java/com/example/async/longPolling/AsyncNumber.java
@@ -1,11 +1,14 @@
 package com.example.async.longPolling;
 
 import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/asyncNumber", asyncSupported = true)
@@ -21,7 +24,29 @@ public class AsyncNumber extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         synchronized (asyncList) {
-            asyncList.add(req.startAsync());
+            AsyncContext asyncContext = req.startAsync();
+            asyncContext.addListener(new AsyncListener() {
+                @Override
+                public void onComplete(AsyncEvent event) {
+                    System.out.println("AsyncEvent onComplete");
+                }
+
+                @Override
+                public void onTimeout(AsyncEvent event) {
+                    System.out.println("AsyncEvent onTimeout");
+                }
+
+                @Override
+                public void onError(AsyncEvent event) {
+                    System.out.println("AsyncEvent onError");
+                }
+
+                @Override
+                public void onStartAsync(AsyncEvent event) {
+                    System.out.println("AsyncEvent onStartAsync");
+                }
+            });
+            asyncList.add(asyncContext);
         }
     }
 }
