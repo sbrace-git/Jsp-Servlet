56681b5c3e96ee92762eb5949e96963360b4eff5
sbrace <sbrace0822@gmail.com>
2022-04-13 00:27:56 +0800

    ch05 async - asyncDownload1

--- ch05/async/src/main/java/com/example/async/download/AsyncDownload.java
+++ ch05/async/src/main/java/com/example/async/download/AsyncDownload.java
@@ -2,15 +2,11 @@ package com.example.async.download;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
