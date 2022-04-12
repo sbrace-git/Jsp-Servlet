2d24c7b56751340f58e15653d0db79d2246b187e
sbrace <sbrace0822@gmail.com>
2022-04-12 22:46:51 +0800

    ch05 async - download

---
+++ ch05/async/src/main/java/com/example/async/download/Download.java
@@ -0,0 +1,24 @@
+package com.example.async.download;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@WebServlet("/download")
+public class Download extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Path path = Paths.get("D:\\common\\temp\\pdf.docx");
+        String mimeType = getServletContext().getMimeType(path.toString());
+        resp.setContentType(mimeType);
+        ServletOutputStream outputStream = resp.getOutputStream();
+        Files.copy(path, outputStream);
+    }
+}
