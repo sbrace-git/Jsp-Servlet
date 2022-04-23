b20ff813f0304ba7984de9a5032271f7bd5ad14e
wly <wly>
2022-04-24 00:50:07 +0800

    ch06 gossip - update index.jsp show login errors

--- ch06/gossip/src/main/java/cc/openhome/gossip/controller/Login.java
+++ ch06/gossip/src/main/java/cc/openhome/gossip/controller/Login.java
@@ -2,56 +2,52 @@ package cc.openhome.gossip.controller;
 
 import cc.openhome.gossip.service.UserService;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 
 @WebServlet(urlPatterns = "/login",
         initParams = {
                 @WebInitParam(name = "SUCCESS_PATH", value = "member"),
-                @WebInitParam(name = "ERROR_PATH", value = "/gossip")
+                @WebInitParam(name = "LOGIN_PATH", value = "/WEB-INF/jsp/index.jsp")
         }
 )
 public class Login extends HttpServlet {
 
     private String SUCCESS_PATH;
 
-    private String ERROR_PATH;
+    private String LOGIN_PATH;
 
     private UserService userService;
 
     @Override
     public void init() {
         SUCCESS_PATH = getInitParameter("SUCCESS_PATH");
-        ERROR_PATH = getInitParameter("ERROR_PATH");
+        LOGIN_PATH = getInitParameter("LOGIN_PATH");
         userService = (UserService) getServletContext().getAttribute("userService");
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-
-        String page;
 
         if (userService.login(username, password)) {
             if (null != req.getSession(false)) {
                 req.changeSessionId();
             }
             req.getSession().setAttribute("login", username);
-            page = SUCCESS_PATH;
+            resp.sendRedirect(SUCCESS_PATH);
         } else {
-            page = ERROR_PATH;
+            ArrayList<String> errors = new ArrayList<>();
+            errors.add("用户名或密码错误");
+            req.setAttribute("errors", errors);
+            req.getRequestDispatcher(LOGIN_PATH).forward(req, resp);
         }
-        resp.sendRedirect(page);
     }
 }
