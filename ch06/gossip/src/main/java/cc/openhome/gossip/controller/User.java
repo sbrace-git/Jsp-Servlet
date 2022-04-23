f6dc7f758de950f411da57a1aff3a2c790444a7c
sbrace <sbrace0822@gmail.com>
2022-04-24 03:14:18 +0800

    ch06 gossip - add controller user.jsp

--- ch06/gossip/src/main/java/cc/openhome/gossip/controller/User.java
+++ ch06/gossip/src/main/java/cc/openhome/gossip/controller/User.java
@@ -12,11 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(urlPatterns = "/user/*",
-        initParams = {
-                @WebInitParam(name = "USER_PATH", value = "/WEB-INF/jsp/user.jsp")
-        }
-)
+//@WebServlet(urlPatterns = "/user/*",
+//        initParams = {
+//                @WebInitParam(name = "USER_PATH", value = "/WEB-INF/jsp/user.jsp")
+//        }
+//)
 public class User extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
