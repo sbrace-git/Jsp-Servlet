a0829776745fa368a31c7fb7c4162509fb2e8e1a
sbrace <sbrace0822@gmail.com>
2022-04-24 23:24:47 +0800

    ch07 JSTL - add login2.jsp

---
+++ ch07/JSTL/src/main/java/com/example/jstl/User.java
@@ -0,0 +1,29 @@
+package com.example.jstl;
+
+public class User {
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean valid() {
+        return "test".equals(username) && "1234".equals(password);
+    }
+}
