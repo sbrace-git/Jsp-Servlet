1d528321d4f0c8f4ec9cecd98d2b16c6ac140676
wly <wly>
2022-06-23 00:12:54 +0800

    ch09 - jdbc-metadata execute sql

--- ch09/jdbc-metadata/src/main/java/com/example/jdbcmetadata/TFilesInfo.java
+++ ch09/jdbc-metadata/src/main/java/com/example/jdbcmetadata/TFilesInfo.java
@@ -1,8 +1,6 @@
 package com.example.jdbcmetadata;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TFilesInfo {
     public static void main(String[] args) {
@@ -15,8 +13,34 @@ public class TFilesInfo {
         String url = "jdbc:h2:D:\\server\\H2\\data\\test";
         String user = "sa";
         String password = "";
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+        String querySql = "select * from t_message";
+        String insert = "insert into t_message values (4,'justin','justin@mail.com','message...')";
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement statement = connection.createStatement()) {
+            if (statement.execute(insert)) {
+                System.out.println("execute return true");
+            } else {
+                int updateCount = statement.getUpdateCount();
+                System.out.printf("updateCount = %d\n", updateCount);
+            }
 
+            if (statement.execute(querySql)) {
+                try (ResultSet resultSet = statement.getResultSet()) {
+                    while (resultSet.next()) {
+                        int id = resultSet.getInt("id");
+                        String name = resultSet.getString("name");
+                        String email = resultSet.getString("email");
+                        String msg = resultSet.getString("msg");
+                        System.out.printf("id = %d\n", id);
+                        System.out.printf("name = %s\n", name);
+                        System.out.printf("email = %s\n", email);
+                        System.out.printf("msg = %s\n", msg);
+                    }
+                }
+            } else {
+                int updateCount = statement.getUpdateCount();
+                System.out.printf("updateCount = %d\n", updateCount);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
