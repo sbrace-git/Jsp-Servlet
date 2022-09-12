9302768e136d3fb52b4e92a8b5a9503c04630a94
sbrace <sbrace0822@gmail.com>
2022-09-12 19:21:41 +0800

    ch09 gossip - ServiceContainer add dataSource

--- ch09/gossip/src/main/java/cc/openhome/gossip/listener/ServiceContainer.java
+++ ch09/gossip/src/main/java/cc/openhome/gossip/listener/ServiceContainer.java
@@ -4,16 +4,29 @@ import cc.openhome.gossip.dao.impl.AccountDaoImpl;
 import cc.openhome.gossip.dao.impl.MessageDaoImpl;
 import cc.openhome.gossip.service.UserService;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
 
 @WebListener
 public class ServiceContainer implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
+        DataSource dataSource;
+        try {
+            InitialContext initialContext = new InitialContext();
+            Context context = (Context) initialContext.lookup("java:/comp/env");
+            dataSource = (DataSource) context.lookup("jdbc/gossip/h2");
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+
         final String USERS = servletContext.getInitParameter("USERS");
         AccountDaoImpl accountDao = new AccountDaoImpl(USERS);
         MessageDaoImpl messageDao = new MessageDaoImpl(USERS);
