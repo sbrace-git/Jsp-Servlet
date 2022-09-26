35a58092c5a422fb183c8686de454b1efe405d46
sbrace <sbrace0822@gmail.com>
2022-09-27 01:18:24 +0800

    ch03 Request - dispatcher include forward

--- ch03/Request/src/main/java/com/example/dispatcher/SomeForward.java
+++ ch03/Request/src/main/java/com/example/dispatcher/SomeForward.java
@@ -8,56 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @WebServlet("/someForward")
 public class SomeForward extends HttpServlet {
 
-    private static final Logger logger = Logger.getLogger(SomeForward.class.getName());
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("\n parameters:");
-        req.getParameterMap().forEach((k, v) ->
-                logger.log(Level.INFO, "{0} = {1}", new Object[]{k, Arrays.toString(v)}));
-
-        logger.info("\n headers:");
-        Collections.list(req.getHeaderNames())
-                .forEach(headerName ->
-                        logger.log(Level.INFO,"{0} = {1}", new Object[]{headerName,req.getHeader(headerName)}));
-
-        logger.log(Level.INFO, "\ncharacterEncoding = {0}\n", req.getCharacterEncoding());
-
-        logger.log(Level.INFO, "contextPath = {0}", req.getContextPath());
-        logger.log(Level.INFO, "requestURL = {0}", req.getRequestURL().toString());
-        logger.log(Level.INFO, "servletPath = {0}", req.getServletPath());
-        logger.log(Level.INFO, "pathInfo = {0}", req.getPathInfo());
-        logger.log(Level.INFO, "queryString = {0}", req.getQueryString());
-        logger.log(Level.INFO, "mapping = {0}", req.getHttpServletMapping().getMappingMatch().name());
-
         PrintWriter writer = resp.getWriter();
         writer.println("Some do one...");
         // writer.flush();
         // java.lang.IllegalStateException
 
-        List<Book> books = Arrays.asList(
-                new Book("《JSP & Servlet学习笔记（第3版）》", "林信良"),
-                new Book("《Java学习笔记》", "林信良")
-        );
-        req.setAttribute("books", books);
-
-        RequestDispatcher dispatcher = req.getRequestDispatcher("other?key=value");
-
-        logger.info("before forward\n");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("other");
 
         dispatcher.forward(req, resp);
-
-        logger.info("after forward");
 
         writer.println("Some do two...");
     }
