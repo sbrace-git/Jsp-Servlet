1032e8febafc1135650dff0a01ca78ed2dc971aa
sbrace <sbrace0822@gmail.com>
2022-10-14 12:24:11 +0800

    init declarative-security

---
+++ ch10/declarative-security/src/main/java/com/example/declarativesecurity/admin/Admin.java
@@ -0,0 +1,29 @@
+package com.example.declarativesecurity.admin;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/admin")
+public class Admin extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doProcess(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doProcess(req, resp);
+    }
+
+    private void doProcess(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            resp.getWriter().println("admin");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
