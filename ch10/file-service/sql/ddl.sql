d8195f577e6277f72e69e6b9e8a15b2ea5c10932
sbrace <sbrace0822@gmail.com>
2022-10-16 23:28:06 +0800

    ch10 file-service - add security

--- ch10/file-service/sql/ddl.sql
+++ ch10/file-service/sql/ddl.sql
@@ -5,4 +5,16 @@ create table image
     create_time date         not null,
     bytes       blob         not null,
     primary key (id)
-)
+);
+create table T_ACCOUNT
+(
+    NAME     CHARACTER VARYING(15) not null
+        primary key,
+    PASSWORD CHARACTER VARYING(32) not null
+);
+create table t_account_role
+(
+    name varchar(15) not null,
+    role varchar(15) not null,
+    primary key (name, role)
+);
