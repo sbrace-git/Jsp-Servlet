4086407d4f312bf713d1ea64e90582ece2936c28
sbrace <sbrace0822@gmail.com>
2022-10-21 13:46:19 +0800

    ch13 gossip update AccountController add reset password

--- ch13/gossip/src/main/java/cc/openhome/gossip/controller/AccountController.java
+++ ch13/gossip/src/main/java/cc/openhome/gossip/controller/AccountController.java
@@ -1,6 +1,9 @@
 package cc.openhome.gossip.controller;
 
+import cc.openhome.gossip.constant.Regex;
+import cc.openhome.gossip.model.Account;
 import cc.openhome.gossip.model.Message;
+import cc.openhome.gossip.service.EmailService;
 import cc.openhome.gossip.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
@@ -23,6 +27,9 @@ public class AccountController {
     private static final String MEMBER_PATH = "member";
     private static final String LOGIN_VIEW_PATH = "/WEB-INF/jsp/index.jsp";
     private static final String LOGIN_PATH = "/gossip";
+    private static final String FORGET_VIEW_PATH = "/WEB-INF/jsp/forgot.jsp";
+    private static final String RESET_PASSWORD_VIEW_PATH = "/WEB-INF/jsp/reset_password.jsp";
+    private static final String RESET_SUCCESS_VIEW_PATH = "/WEB-INF/jsp/reset_success.jsp";
 
     @PostMapping("/login")
     public void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -61,5 +68,66 @@ public class AccountController {
         req.getSession().invalidate();
         req.logout();
         resp.sendRedirect(LOGIN_PATH);
+    }
+
+    @PostMapping("/forgot")
+    public void forgot(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        UserService userService = (UserService) req.getServletContext().getAttribute("userService");
+        EmailService emailService = (EmailService) req.getServletContext().getAttribute("emailService");
+        Optional<Account> accountByNameEmail = userService.getAccountByNameEmail(name, email);
+        accountByNameEmail.ifPresent(emailService::passwordResetLink);
+        req.setAttribute("email", email);
+        req.getRequestDispatcher(FORGET_VIEW_PATH).forward(req, resp);
+    }
+
+    @GetMapping("/reset-password")
+    public void resetPasswordForm(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException, ServletException {
+        String name = req.getParameter("name");
+        String token = req.getParameter("token");
+        String email = req.getParameter("email");
+        UserService userService = (UserService) req.getServletContext().getAttribute("userService");
+        Optional<Account> accountByEmail = userService.getAccountByNameEmail(name, email);
+        if (accountByEmail.isPresent()) {
+            Account account = accountByEmail.get();
+            if (account.getPassword().equals(token)) {
+                req.setAttribute("account", account);
+                req.getSession().setAttribute("token", token);
+                req.getRequestDispatcher(RESET_PASSWORD_VIEW_PATH).forward(req, resp);
+                return;
+            }
+        }
+        resp.sendRedirect(LOGIN_PATH);
+    }
+
+    @PostMapping("/reset-password")
+    public void resetPassword(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String token = req.getParameter("token");
+        String storedToken = (String) req.getSession().getAttribute("token");
+        if (null == storedToken || !storedToken.equals(token)) {
+            resp.sendRedirect("/gossip");
+            return;
+        }
+
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String password2 = req.getParameter("password2");
+
+        if (null == password
+                || !Regex.passwdRegex.matcher(password).find()
+                || !password.equals(password2)) {
+            req.setAttribute("errors", Collections.singletonList("请确认密码符合格式并再度确认密码"));
+            req.getRequestDispatcher(RESET_PASSWORD_VIEW_PATH).forward(req, resp);
+            return;
+        }
+        UserService userService = (UserService) req.getServletContext().getAttribute("userService");
+        Optional<Account> accountByNameEmail = userService.getAccountByNameEmail(name, email);
+        req.setAttribute("account", accountByNameEmail.get());
+        userService.resetPassword(name, password);
+        req.getRequestDispatcher(RESET_SUCCESS_VIEW_PATH).forward(req, resp);
+
     }
 }
