fa9965ee0f497284398afca91260603df990d35c
sbrace <sbrace0822@gmail.com>
2022-10-21 13:31:26 +0800

    ch13 gossip add AccountController

--- ch13/gossip/src/main/java/cc/openhome/gossip/controller/AccountController.java
+++ ch13/gossip/src/main/java/cc/openhome/gossip/controller/AccountController.java
@@ -2,11 +2,11 @@ package cc.openhome.gossip.controller;
 
 import cc.openhome.gossip.model.Message;
 import cc.openhome.gossip.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -16,32 +16,20 @@ import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@WebServlet(urlPatterns = "/login",
-        initParams = {
-                @WebInitParam(name = "SUCCESS_PATH", value = "member"),
-                @WebInitParam(name = "LOGIN_PATH", value = "/WEB-INF/jsp/index.jsp")
-        }
-)
-public class LoginController extends HttpServlet {
+@Controller
+public class AccountController {
+    private final Logger logger = Logger.getLogger(AccountController.class.getName());
 
-    private final Logger logger = Logger.getLogger(LoginController.class.getName());
+    private static final String MEMBER_PATH = "member";
+    private static final String LOGIN_VIEW_PATH = "/WEB-INF/jsp/index.jsp";
+    private static final String LOGIN_PATH = "/gossip";
 
-    private static String SUCCESS_PATH;
-    private static String LOGIN_PATH;
-    private UserService userService;
-
-    @Override
-    public void init() {
-        SUCCESS_PATH = getInitParameter("SUCCESS_PATH");
-        LOGIN_PATH = getInitParameter("LOGIN_PATH");
-        userService = (UserService) getServletContext().getAttribute("userService");
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @PostMapping("/login")
+    public void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
+        UserService userService = (UserService) req.getServletContext().getAttribute("userService");
         Optional<String> optionalPasswd = userService.encryptedPassword(username, password);
 
         try {
@@ -55,7 +43,7 @@ public class LoginController extends HttpServlet {
                 req.changeSessionId();
             }
             req.getSession().setAttribute("login", username);
-            resp.sendRedirect(SUCCESS_PATH);
+            resp.sendRedirect(MEMBER_PATH);
             logger.log(Level.INFO, "{0} login success", username);
         } catch (Exception e) {
             logger.log(Level.WARNING, "login error", e);
@@ -64,7 +52,14 @@ public class LoginController extends HttpServlet {
             ArrayList<String> errors = new ArrayList<>();
             errors.add("登录失败");
             req.setAttribute("errors", errors);
-            req.getRequestDispatcher(LOGIN_PATH).forward(req, resp);
+            req.getRequestDispatcher(LOGIN_VIEW_PATH).forward(req, resp);
         }
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        req.logout();
+        resp.sendRedirect(LOGIN_PATH);
     }
 }
