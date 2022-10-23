3463935521da9200bd9a74dd45618bcd51bc95ac
sbrace <sbrace0822@gmail.com>
2022-10-23 23:35:34 +0800

    ch14 hello init

---
+++ ch14/hello/src/main/java/com/example/hello/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(String name) {
+        return String.format("哈喽！%s!", name);
+    }
+}
