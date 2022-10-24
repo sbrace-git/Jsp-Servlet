33267420061430ff32cb8fdc65d63fc33c3f2bc1
sbrace <sbrace0822@gmail.com>
2022-10-24 15:30:07 +0800

    ch13 ch14 add init.sql

---
+++ ch14/gossip/sql/init.sql
@@ -0,0 +1,23 @@
+create table T_ACCOUNT
+(
+    NAME     CHARACTER VARYING(15)  not null
+        primary key,
+    EMAIL    CHARACTER VARYING(128) not null,
+    PASSWORD CHARACTER VARYING(225)  not null,
+    SALT     CHARACTER VARYING(256) not null
+);
+
+create table T_MESSAGE
+(
+    USERNAME CHARACTER VARYING(15)  not null
+        references T_ACCOUNT,
+    MILLIS   BIGINT                 not null,
+    BLABLA   CHARACTER VARYING(512) not null
+);
+
+create table t_account_role
+(
+    name varchar(15) not null,
+    role varchar(15) not null,
+    primary key (name, role)
+);
